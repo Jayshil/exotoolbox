3dd4eb68d12f34ab5526d6a6b9940ed9b17afc08
nespinoza <nestor.espinozap@gmail.com>
2017-03-27 16:01:37 -0300

    Aded init and setup.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='exotoolbox',
+      version='0.1',
+      description='The Exoplaneteers Toolbox',
+      url='http://github.com/nespinoza/exotoolbox',
+      author='Nestor Espinoza',
+      author_email='nsespino@uc.cl',
+      license='MIT',
+      packages=['plots','utils'],
+      zip_safe=False)
