2e7492f96044c2f5e684d1f092ca0de394b9fcbd
Jayshil <jayshilpatel147@gmail.com>
2023-05-12 15:11:11 +0200

    version change

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='exotoolbox',
-      version='0.1',
+      version='0.1.1',
       description='The Exoplaneteers Toolbox',
       url='http://github.com/nespinoza/exotoolbox',
       author='Nestor Espinoza',
       author_email='nsespino@uc.cl',
       license='MIT',
       packages=['exotoolbox'],
-      install_requires=['batman-package','emcee','lmfit'],
+      install_requires=['batman-package','emcee','lmfit','astropy','corner'],
       zip_safe=False)
